5cd25e070b8fbd5a826fc578ee807f1c9a04d00f
Matthew Adams <matthew.adams@endjin.com>
2022-12-04 08:57:36 +0000

    Added Day 4.

---
+++ Day4/Elf.Assignments/ElfAccumulator.cs
@@ -0,0 +1,44 @@
+﻿namespace Elf.Assignments;
+
+using System;
+
+internal ref struct ElfAccumulator
+{
+    private int lineNumber;
+
+    public ElfAccumulator()
+    {
+        this.OverlapCount = 0;
+    }
+
+    public int OverlapCount { get; private set; }
+
+    public void ProcessLine(string line)
+    {
+        this.lineNumber++;
+        if (this.IsFullyContainedOverlap(line))
+        {
+            this.OverlapCount++;
+        }
+    }
+
+    private bool IsFullyContainedOverlap(string line)
+    {
+        ReadOnlySpan<char> lineSpan = line.AsSpan();
+        // There must be at least 1 digit, so we don't need to look in the first char
+        int firstSeparator = lineSpan[1..].IndexOf('-') + 1;
+        // There must be at least 1 digit, so we don't need to look in the first char after the separator
+        int rangeSeparator = lineSpan[(firstSeparator + 2)..].IndexOf(',') + (firstSeparator + 2);
+        // There must be at least 1 digit, so we don't need to look in the first char after the separator
+        int secondSeparator = lineSpan[(rangeSeparator + 1)..].IndexOf('-') + (rangeSeparator + 1);
+
+        int start1 = int.Parse(lineSpan[0..firstSeparator]);
+        int end1 = int.Parse(lineSpan[(firstSeparator + 1)..rangeSeparator]);
+        int start2 = int.Parse(lineSpan[(rangeSeparator + 1)..secondSeparator]);
+        int end2 = int.Parse(lineSpan[(secondSeparator + 1)..]);
+
+        return
+            (start1 <= start2 && end1 >= end2) ||
+            (start2 <= start1 && end2 >= end1);
+    }
+}
---
+++ Day4/Elf.Assignments/Program.cs
@@ -0,0 +1,30 @@
+﻿using Elf.Assignments;
+using System.CommandLine;
+
+var fileOption = new Option<FileInfo>(
+    name: "--file",
+    description: "The file containing the elf assignments.");
+
+
+var rootCommand = new RootCommand("Advent of Code Day 4 Assignments");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler(
+    ProcessElfFile,
+    fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static void ProcessElfFile(FileInfo file)
+{
+    ElfAccumulatorPartialOverlap accumulator = new();
+
+    // We could switch this to read lines of char/utf8byte out of the buffer and avoid
+    // allocating all the strings, and process a ReadOnlySpan<char> or ReadOnlySpan<byte> (depending on the encoding)
+    foreach (var line in File.ReadLines(file.FullName))
+    {
+        accumulator.ProcessLine(line);
+    }
+
+    Console.WriteLine($"The count of partially overlapping assigments was {accumulator.OverlapCount}.");
+}
