06c98547f22daca92f1fde2e644985398daeaa9e
Matthew Adams <matthew.adams@endjin.com>
2022-12-05 10:11:46 +0000

    Changed namespace style.

--- Day5/Elf.SupplyStacks/CrateStack.cs
+++ Day5/Elf.SupplyStacks/CrateStack.cs
@@ -1,68 +1,67 @@
-﻿namespace Elf.SupplyStacks
+﻿namespace Elf.SupplyStacks;
+
+using System;
+
+/// <summary>
+/// This is a stack that supports pushing and popping substacks
+/// and shifting values into the bottom of the stack for intitialization.
+/// </summary>
+public readonly struct CrateStack
 {
-    using System;
+    private readonly List<char> stack;
+
+    public CrateStack()
+    {
+        this.stack = new();
+    }
+
+    public int Count => this.stack.Count;
 
     /// <summary>
-    /// This is a stack that supports pushing and popping substacks
-    /// and shifting values into the bottom of the stack for intitialization.
+    /// Shifts a value into the bottom of the stack
     /// </summary>
-    public readonly struct CrateStack
+    /// <param name="value">The value to insert.</param>
+    public void Shift(char value)
     {
-        private readonly List<char> stack;
+        this.stack.Insert(0, value);
+    }
 
-        public CrateStack()
+    public char Peek()
+    {
+        return this.stack[^1];
+    }
+
+    public void Push(char value)
+    {
+        this.stack.Add(value);
+    }
+
+    public void Push(ReadOnlySpan<char> value)
+    {
+        foreach(char item in value)
         {
-            this.stack = new();
+            this.stack.Add(item);
         }
+    }
 
-        public int Count => this.stack.Count;
+    /// <summary>
+    /// Pops an item from the stack
+    /// </summary>
+    /// <returns>The item that was at the top of the stack</returns>
+    public char Pop()
+    {
+        var result = this.stack[^1];
+        this.stack.RemoveAt(this.stack.Count - 1);
+        return result;
+    }
 
-        /// <summary>
-        /// Shifts a value into the bottom of the stack
-        /// </summary>
-        /// <param name="value">The value to insert.</param>
-        public void Shift(char value)
-        {
-            this.stack.Insert(0, value);
-        }
-
-        public char Peek()
-        {
-            return this.stack[^1];
-        }
-
-        public void Push(char value)
-        {
-            this.stack.Add(value);
-        }
-
-        public void Push(ReadOnlySpan<char> value)
-        {
-            foreach(char item in value)
-            {
-                this.stack.Add(item);
-            }
-        }
-
-        /// <summary>
-        /// Pops an item from the stack
-        /// </summary>
-        /// <returns>The item that was at the top of the stack</returns>
-        public char Pop()
-        {
-            var result = this.stack[^1];
-            this.stack.RemoveAt(this.stack.Count - 1);
-            return result;
-        }
-
-        public ReadOnlySpan<char> Pop(int numberToPop)
-        {
-            // We need to make a copy of the result
-            int index = this.stack.Count - numberToPop;
-            var span = this.stack.ToArray().AsSpan().Slice(index, numberToPop);
-            // And clear it from the original stack
-            this.stack.RemoveRange(index, numberToPop);
-            return span;
-        }
+    public ReadOnlySpan<char> Pop(int numberToPop)
+    {
+        // We need to make a copy of the result
+        int index = this.stack.Count - numberToPop;
+        var span = this.stack.ToArray().AsSpan().Slice(index, numberToPop);
+        // And clear it from the original stack
+        this.stack.RemoveRange(index, numberToPop);
+        return span;
     }
 }
