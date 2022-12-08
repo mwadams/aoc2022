604e5c9c501e212a3902df2f1db851b18c41769c
Matthew Adams <matthew.adams@endjin.com>
2022-12-08 20:33:54 +0000

    Prepping for day 9

---
+++ Day9/Elf.Shenanigans/ElfAccumulator.cs
@@ -0,0 +1,15 @@
+﻿namespace Elf.Shenanigans;
+
+using System;
+
+internal ref struct ElfAccumulator
+{
+
+    public ElfAccumulator()
+    {
+    }
+
+    public void ProcessLine(string line)
+    {
+    }
+}
---
+++ Day9/Elf.Shenanigans/Program.cs
@@ -0,0 +1,26 @@
+﻿using Elf.Shenanigans;
+using System.CommandLine;
+
+var fileOption = new Option<FileInfo>(
+    name: "--file",
+    description: "The file containing the day 9 input.");
+
+
+var rootCommand = new RootCommand("Advent of Code Day 9");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler(
+    ProcessElfFile,
+    fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static void ProcessElfFile(FileInfo file)
+{
+    foreach (var line in File.ReadLines(file.FullName))
+    {
+        ElfAccumulator accumulator = new();
+        accumulator.ProcessLine(line);
+        Console.WriteLine($"The output was...");
+    }
+}
