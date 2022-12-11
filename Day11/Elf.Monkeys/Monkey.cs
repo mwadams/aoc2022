fd54f450cbfd54b95a6ac478418a234f0ed87562
Matthew Adams <matthew.adams@endjin.com>
2022-12-11 23:31:14 +0000

    Added timings and removed unused method.

--- Day11/Elf.Monkeys/Monkey.cs
+++ Day11/Elf.Monkeys/Monkey.cs
@@ -1,8 +1,2 @@
 ﻿namespace Elf.Monkeys;
-internal record struct Monkey(List<ulong> Items, Func<ulong, ulong> Operation, Predicate<ulong> Test, ulong TestValue, int TrueTarget, int FalseTarget)
-{
-    internal void AddItem(ulong worryLevel)
-    {
-        this.Items.Add(worryLevel);
-    }
-}
+internal record struct Monkey(List<ulong> Items, Func<ulong, ulong> Operation, Predicate<ulong> Test, ulong TestValue, int TrueTarget, int FalseTarget);
--- Day11/Elf.Monkeys/Program.cs
+++ Day11/Elf.Monkeys/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Elf.Monkeys;
 using System.CommandLine;
+using System.Diagnostics;
 
 var fileOption = new Option<FileInfo>(
     name: "--file",
@@ -15,12 +16,33 @@ rootCommand.SetHandler(
 
 return await rootCommand.InvokeAsync(args);
 
+const int Iterations = 100;
+
 static void ProcessElfFile(FileInfo file)
 {
-    string[] program = File.ReadAllLines(file.FullName);
-    ElfAccumulator accumulator = new(program);
+    ElfAccumulator accumulator = default;
 
-    accumulator.Play(10000, 2, false);
+    var sw = Stopwatch.StartNew();
+    for (int i = 0; i < Iterations; ++i)
+    {
+        string[] program = File.ReadAllLines(file.FullName);
+        accumulator = new(program);
+        accumulator.Play(20, 2, true);
+    }
+    sw.Stop();
 
     Console.WriteLine($"{accumulator.MonkeyBusiness}");
+    Console.WriteLine($"{sw.ElapsedMilliseconds / (double)Iterations}ms");
+
+    sw = Stopwatch.StartNew();
+    for (int i = 0; i < Iterations; ++i)
+    {
+        string[] program = File.ReadAllLines(file.FullName);
+        accumulator = new(program);
+        accumulator.Play(10000, 2, false);
+    }
+    sw.Stop();
+
+    Console.WriteLine($"{accumulator.MonkeyBusiness}");
+    Console.WriteLine($"{sw.ElapsedMilliseconds / (double)Iterations}ms");
 }
