fc4a9c8143270659bf50bd090447c3a495f79503
Matthew Adams <matthew.adams@endjin.com>
2022-12-12 23:59:26 +0000

    Initial setup.

---
+++ Day13/Elf.Larks/ElfAccumulatorPt1.cs
@@ -0,0 +1,13 @@
+﻿namespace Elf.Larks;
+
+using System.Collections.Generic;
+
+internal ref struct ElfAccumulatorPt1
+{
+    private readonly string[] lines;
+
+    public ElfAccumulatorPt1(string[] lines)
+    {
+        this.lines = lines;
+    }
+}
---
+++ Day13/Elf.Larks/Program.cs
@@ -0,0 +1,23 @@
+﻿using Elf.Larks;
+using System.CommandLine;
+
+var fileOption = new Option<FileInfo>(
+    name: "--file",
+    description: "The file containing the elf supply stacks.");
+
+
+var rootCommand = new RootCommand("Advent of Code Day 5");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler(
+    ProcessElfFile,
+    fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static void ProcessElfFile(FileInfo file)
+{
+    string[] lines = File.ReadAllLines(file.FullName);
+
+    ElfAccumulatorPt1 accumulator = new(lines);
+}
