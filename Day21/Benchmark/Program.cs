dd22d31b4b1e3d4ee3ccc9cf7d1c254443680cbb
Matthew Adams <matthew.adams@endjin.com>
2022-12-21 11:44:27 +0000

    Added benchmarking.

---
+++ Day21/Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Perfolizer.Mathematics.OutlierDetection;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll(
+        ManualConfig.Create(DefaultConfig.Instance)
+        .AddJob(Job.Dry
+            .WithRuntime(CoreRuntime.Core70)
+            .WithOutlierMode(OutlierMode.RemoveAll)
+            .WithStrategy(RunStrategy.Throughput)
+            .WithIterationCount(1)));
