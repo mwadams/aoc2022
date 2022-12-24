137afbb3d0ab9b6beab84c190c17d5c558e661e6
Matthew Adams <matthew.adams@endjin.com>
2022-12-24 18:28:39 +0000

    Added benchmarks

---
+++ Day24/Benchmark/Part1.cs
@@ -0,0 +1,33 @@
+﻿namespace Benchmarking;
+
+using BenchmarkDotNet.Attributes;
+using Elf.Dodgems;
+
+/// <summary>
+/// Construct elements from a JSON element.
+/// </summary>
+[MemoryDiagnoser]
+public class Part1
+{
+    private string[] lines = Array.Empty<string>();
+    private long result;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        this.lines = File.ReadAllLines("./input.txt");
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        Console.WriteLine($"Pt1: {this.result}");
+    }
+
+    [Benchmark(Baseline = true)]
+    public void RunPart1()
+    {
+        ElfAccumulator accumulator = new(lines);
+        this.result = accumulator.Process();
+    }
+}
